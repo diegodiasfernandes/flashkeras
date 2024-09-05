0e3ba833cc0acdfcf255f5b6cfadee15c91c315b
Diego Dias <diegodiasfernandes.comp@gmail.com>
2024-09-05 16:21:02 -0300

    get_dummies

--- preprocessing/FlashPreProcessing.py
+++ preprocessing/FlashPreProcessing.py
@@ -27,33 +27,13 @@ class FlashPreProcessing:
     @staticmethod
     def ensureOneHotEncoding(
                 y: Union[np.ndarray, pd.Series]
-                ) -> Union[np.ndarray, pd.Series]:
+                ):
         
-        new_y = y
+        if isinstance(y, np.ndarray):
+            y = pd.Series(y)
+            return pd.get_dummies(y).values
         
-        if isinstance(new_y, pd.Series):
-            num_classes = len(new_y.unique())
-        elif isinstance(new_y, np.ndarray):
-            if new_y.ndim == 1:  # Case where y is a 1D array of labels
-                num_classes = len(np.unique(new_y))
-            elif new_y.ndim == 2:  # Case where y is one-hot encoded
-                num_classes = new_y.shape[1]
-        else:
-            raise ValueError("Unsupported y type.")
-                
-        if num_classes == 2:
-            if isinstance(new_y, pd.Series) or isinstance(new_y, np.ndarray):
-                if new_y.ndim == 2 and new_y.shape[1] == 1:
-                    new_y = new_y.ravel()  # Flatten y if it's a 2D array with one column
-                if np.unique(new_y).tolist() != [0, 1]:
-                    new_y = cast(np.ndarray, (new_y == np.max(new_y)).astype(int))  # Ensure y is binary (0 and 1)
-                        
-        else:
-            if isinstance(new_y, pd.Series) or isinstance(new_y, np.ndarray):
-                if new_y.ndim == 1 or new_y.shape[1] == 1:  # If y is not one-hot encoded
-                    new_y = np.eye(num_classes)[new_y.astype(int)]
-
-        return new_y
+        return pd.get_dummies(y).values
     
     @staticmethod
     def datasetToArray(
