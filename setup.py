0e0a5979f4dafa485dbde73ef7b33ace0ea8758d
Diego Dias <diegodiasfernandes.comp@gmail.com>
2024-09-17 13:18:52 -0300

    toml

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="flashkeras",
-    version="0.1",
+    version="0.1.0",
     packages=find_packages(include=['flashkeras', 'flashkeras.*']),
     install_requires=[ 
         'tensorflow==2.15.0',
