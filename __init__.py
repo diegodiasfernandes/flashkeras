d42efd7ae66701139784cfd81b0bd49fa901b784
Diego Dias <diegodiasfernandes.comp@gmail.com>
2024-09-17 12:16:21 -0300

    init update

--- __init__.py
+++ __init__.py
@@ -2,7 +2,6 @@ from .models import FlashSequential, FlashTransferLearning
 from .preprocessing import FlashPreProcessing, FlashDataGenerator
 
 __all__ = [
-           'PreProcessingClass',
            'FlashSequential',
            'FlashTransferLearning',
            'FlashPreProcessing',
