15bc9800915d4a7c4745270f5012de77d3f39dae
Diego Dias <diegodiasfernandes.comp@gmail.com>
2024-09-24 10:25:11 -0300

    readcsv and readtable are part of data collecting init

--- flashkeras/data_collecting/__init__.py
+++ flashkeras/data_collecting/__init__.py
@@ -1,5 +1,8 @@
 from flashkeras.preprocessing.images.FlashDataGenerator import FlashDataGenerator
+from pandas import read_csv, read_table
 
 __all__ = [
-    'FlashDataGenerator'
+    'FlashDataGenerator',
+    'read_csv',
+    'read_table'
 ]
