927cb8e7d0b9347c5f15729221733aed5b0fe023
Diego Dias <diegodiasfernandes.comp@gmail.com>
2024-09-24 10:27:52 -0300

    main init update

--- flashkeras/__init__.py
+++ flashkeras/__init__.py
@@ -2,10 +2,14 @@ from .models.FlashSequential import FlashSequential
 from .models.FlashTransferLearning import FlashTransferLearning
 from .preprocessing.FlashPreProcessing import FlashPreProcessing
 from .preprocessing.images.FlashDataGenerator import FlashDataGenerator
+from .evaluation.FlashEvaluating import FlashEvaluating
+import pandas
+import numpy
 
 __all__ = [
            'FlashSequential',
            'FlashTransferLearning',
            'FlashPreProcessing',
-           'FlashDataGenerator'
+           'FlashDataGenerator',
+           'FlashEvaluating'
 ]
