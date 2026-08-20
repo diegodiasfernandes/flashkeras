04f89d7752eafe08c95e7c77a094857104d19ff5
Diego Dias <diegodiasfernandes.comp@gmail.com>
2026-08-20 10:08:33 -0300

    fix: first version with image_classification_basline example notebook working

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages # type: ignore
 
 setup(
     name="flashkeras",
-    version="0.1.8",
+    version="0.1.9",
     packages=find_packages(include=['flashkeras', 'flashkeras.*']),
     install_requires=[ 
         'tensorflow==2.15.0',
