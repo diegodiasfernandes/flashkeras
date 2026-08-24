c16b17e210b5c2e69e4bc2485e96e36e287f261e
Diego Dias <diegodiasfernandes.comp@gmail.com>
2026-08-24 08:52:50 -0300

    feat: first functional version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages # type: ignore
 
 setup(
     name="flashkeras",
-    version="0.1.9",
+    version="1.0.0",
     packages=find_packages(include=['flashkeras', 'flashkeras.*']),
     install_requires=[ 
         'tensorflow==2.15.0',
