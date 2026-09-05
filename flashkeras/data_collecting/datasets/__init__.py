5df71c045c07ad0e8fe9095e49c13af270db5e72
Diego Dias <diegodiasfernandes.comp@gmail.com>
2026-09-05 11:34:24 -0300

    feat: reorganize dataset loading functions into separate modules for tabular and text classification

--- flashkeras/data_collecting/datasets/__init__.py
+++ flashkeras/data_collecting/datasets/__init__.py
@@ -1,6 +1,6 @@
-from .dataframes import load_wine, load_breast_cancer, load_diabetes, load_iris, boston_housing_load_data
+from .tabular import load_wine, load_breast_cancer, load_diabetes, load_iris, boston_housing_load_data
 from .image import load_cifar100, load_mnist, load_cifar10, load_fashion_mnist, load_digits
-from .matrix_like import load_imdb, imdb_get_world_index, load_reuters, reuters_get_word_index, reuters_get_label_names
+from .text_classification import load_imdb, imdb_get_world_index, load_reuters, reuters_get_word_index, reuters_get_label_names
 
 __all__ = [
     'load_wine', 
